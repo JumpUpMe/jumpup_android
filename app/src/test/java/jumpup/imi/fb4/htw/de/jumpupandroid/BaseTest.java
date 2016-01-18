cc3c345eed686c4f5aa810e903f207f98aa7453f
Sascha Feldmann <sascha.feldmann@gmx.de>
2016-01-18 12:22:05 +0100

    refactoring project structure, added Travis build file

---
+++ app/src/test/java/jumpup/imi/fb4/htw/de/jumpupandroid/BaseTest.java
@@ -0,0 +1,12 @@
+package jumpup.imi.fb4.htw.de.jumpupandroid;
+
+/**
+ * Project: jumpup_android
+ * <p/>
+ * [short_description]
+ *
+ * @author Sascha Feldmann <a href="mailto:sascha.feldmann@gmx.de">sascha.feldmann@gmx.de</a>
+ * @since 18.01.2016
+ */
+public abstract class BaseTest {
+}
